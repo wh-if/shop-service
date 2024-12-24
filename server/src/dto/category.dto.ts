29dcdd990b254444c9fbba892a12a5cd1cedb372
wh-if <884923319@qq.com>
2024-12-24 09:48:46 +0800

    refactor: 调整参数校验方法位置

--- server/src/dto/category.dto.ts
+++ server/src/dto/category.dto.ts
@@ -1,4 +1,6 @@
+import { BadRequestException } from '@nestjs/common';
 import { ListOrderType, ListQueryParam } from 'src/common/type';
+import { Validator } from 'src/common/validator';
 import { Category } from 'src/entity/category.entity';
 
 export type CategoryUpdateDTO = Category;
@@ -11,3 +13,22 @@ export type CategoryListQueryDTO = ListQueryParam<
 >;
 
 export type CategoryListOrderDTO = ListOrderType<Category, 'id'>;
+
+export function validateDTO(
+  dto: CategoryInsertDTO | CategoryUpdateDTO,
+  isUpdate: 'insert' | 'update',
+) {
+  try {
+    if (isUpdate === 'update') {
+      Validator.required((dto as CategoryUpdateDTO).id, 'id').number();
+      Validator.unRequired(dto.name, 'name').string().max(16);
+    } else {
+      Validator.required(dto.name, 'name').string().max(16);
+    }
+
+    Validator.unRequired(dto.avatar, 'avatar').string().max(255);
+    Validator.unRequired(dto.parentId, 'parentId').number();
+  } catch (error) {
+    throw new BadRequestException(error.message);
+  }
+}
